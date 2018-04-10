288a611db881082960bb6d70b92cbd36a365478a
ChrisChenDuo <chd_offi_hi@aliyun.com>
2018-04-10 10:25:47 +0800

    2018.4.10

--- ProxyPool/zhandaye.py
+++ ProxyPool/zhandaye.py
@@ -8,21 +8,20 @@ s.headers = {
     'Accept-Language':'zh-CN,zh;q=0.9',
     'Cache-Control':'max-age=0',
     'Connection':'keep-alive',
-    'Cookie':'free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJTZiNGQ1ZTU3YjMwZTA2ZGYxMzVhY2M3YjZkNDkxNGQ5BjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMWhVR1VKdk5hZWdKcitEQW5sSm1sanBJY3htbVZmU0JUMWQ3OFdhck44clk9BjsARg%3D%3D--60da3d0d1ed32e7e62ae84aab7ca86e678536a51; Hm_lvt_0cf76c77469e965d2957f0553e6ecf59=1521345442,1522929663; Hm_lpvt_0cf76c77469e965d2957f0553e6ecf59=1522929663',
-    'Host':'www.xicidaili.com',
-    'If-None-Match': 'W/"81a4a5c3279ec77139d18a237910610a"',
-    'Referer': 'https://www.baidu.com/link?url=QdQlW7k_y7eJ6YggypAR1U6c-DT7cad3U2BzGygm0aSurUjgEYAeedNnvu5QfjUS&wd=&eqid=a954efc500072df6000000035ac60ff9',
+    'Cookie':'acw_tc=AQAAAMgkBm5yig4AxaHzdJ8ES8UguQSn; ASPSESSIONIDCQBTCTRB=OKHHPOJBELJOBPNJKHLPMELH; __51cke__=; ASPSESSIONIDAQAQDQTB=EEDHANKBHOEJIKENMMPLNACB; ASPSESSIONIDAQDSARSA=CDAEBELBOALMKPJJIDEJHPDN; acw_sc__=5acb6c95550452cbcfcf69a9413a07e22879276d; Hm_lvt_8fd158bb3e69c43ab5dd05882cf0b234=1523279241,1523279255,1523281047,1523281054; Hm_lpvt_8fd158bb3e69c43ab5dd05882cf0b234=1523281054; __tins__16949115=%7B%22sid%22%3A%201523281379946%2C%20%22vd%22%3A%204%2C%20%22expires%22%3A%201523283276184%7D; __51laig__=25',
+    'Host':'ip.zdaye.com',
+    'Referer': 'http://ip.zdaye.com/',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36'
 }
 
 #访问
-response = s.get(url='http://www.xicidaili.com/')
+response = s.get(url='http://ip.zdaye.com/')
 
 #返回码
 print(response.status_code)
 #返回结果
-html_text = response.content.decode(encoding='utf-8')
+html_text = response.content.decode(encoding='gbk')
 #print(html_text)
 
 
@@ -83,42 +82,31 @@ def parse(func):
 #再重构：我们直接使用@装饰 看看是否可以在parse中避免首尾两行代码。
 @parse
 def find_elements(etree):
-    elements = []
-    TRs = etree.xpath('//tr')
-    #国内高匿https
-    HighHiding = 0
-    #print(TRs)
-    for i,tr in enumerate(TRs):
-        #print(tr)
-        th = tr.xpath('th')
-        h2 = []
-        if len(th):
-            h2 = th[0].xpath('h2')
-            if len(h2):
-                print(h2[0].text)
-        #print(h2)
-        if HighHiding == 0:
-            if len(h2) and h2[0].text == "国内高匿代理IP":
-                HighHiding = 1
-                continue
-            else:
-                continue
-        if len(h2) and h2[0].text == "国内透明代理IP":
-            break
+    Tbodys = etree.xpath('//tbody')
+    TRs = Tbodys[0].xpath('tr')
+    for i, tr in enumerate(TRs):
         TDs = tr.xpath('td')
-        #print(TDs)
-        #print(TDs[1].text, TDs[1].tag)
-        if len(TDs) and TDs[5].text == "HTTPS":
-            #print(TDs[1].text, TDs[2].text)
-            yield {'ip':TDs[1].text, 'port':TDs[2].text}
-    #HighHiding = TRs[start:end+1]
-    #HHHttps = []
-
-    #print(buf)
-    #elements.append({'element':buf[1], 'about':'text', 'which':None})
-    #return elements
-
+        https = TDs[6].xpath('div')
+        if len(https):
+            if '高匿' == TDs[3].text:
+                yield {'ip':TDs[0].text, 'port':TDs[2].xpath('img/@src')}
+def down_pic(url='https://up.enterdesk.com/edpic_source/ca/6b/5f/ca6b5f366b3ea52aab975235096594e8.jpg'):
+    res = requests.get(url)
+    f = open('test.jpg', 'wb')
+    f.write(res.content)
+    f.close()
 ProxyGenerater = find_elements(html_text)
-for proxy in ProxyGenerater:
-    print(proxy['ip'], proxy['port'])
+for i, proxy in enumerate(ProxyGenerater):
+    print(proxy['ip'])
+    PicUrl = 'http://ip.zdaye.com'+proxy['port'][0]
+    #PicUrl = 'https://up.enterdesk.com/edpic_source/ca/6b/5f/ca6b5f366b3ea52aab975235096594e8.jpg'
+    #print(proxy['port'][0])
+    s.headers['referer'] = 'http://ip.zdaye.com/'
+    r = s.get(url=PicUrl)
+    down_pic(PicUrl)
+    #print(r.content.decode(encoding='utf-8',errors='ignore'))
+    f = open('N'+'1'*i+'.jpg', 'wb')
+    f.write(r.content)
+    f.close()
+
 
